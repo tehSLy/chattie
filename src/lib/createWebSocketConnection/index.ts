a03cffb521e44018583832619dc994a58720f5a1
tehSLy <sly5992@gmail.com>
2019-08-07 15:52:21 +0300

    Update index.ts

--- src/lib/createWebSocketConnection/index.ts
+++ src/lib/createWebSocketConnection/index.ts
@@ -4,20 +4,13 @@ export const createWebSocketConnection = (url: string) => {
 	const instance = new WebSocket(url);
 
 	const sendMessage = createEffect<object, any, Error>().use((data) => instance.send(JSON.stringify(data)));
-	const events = createEvents(instance);
-	const status = createStore<keyof WebSocketEventMap>("close")
-		.on(events.close, () => "close")
-		.on(events.open, () => "open")
-		.on(events.error, () => "error");
+	const statusChanged = createEvents(instance);
+	const status = restore(statusChanged, "close");
 
-	return { sendMessage, instance, events, status };
+	return { sendMessage, instance, statusChanged, status };
 };
 
 const createEvents = (instance: WebSocket) => {
-	return ["message", "error", "close", "open"].reduce((carry: any, key) => {
-		const event = createEvent<Event>();
-		instance.addEventListener(key, event);
-		carry[key] = event;
-		return carry;
-	}, {}) as { [key in keyof WebSocketEventMap]: EffectorEvent<Event> };
+	const event = createEvent<Event>();
+	return ["message", "error", "close", "open"].map((key) => instance.addEventListener(key, event));
 };
