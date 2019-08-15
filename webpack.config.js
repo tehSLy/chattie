12b86131194dcde4daeb025b932e4a6c4745d809
SLy <sly5992@gmail.com>
2019-08-15 22:20:12 +0300

    public folder added

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
 
 	output: {
 		filename: "[name].[chunkhash].js",
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "public"),
 		publicPath: '/'
 	},
 
